38d6b7634322dd52674739dfbb9a513ab8da28f1
iykeori <oricletus@gmail.com>
2024-02-05 16:49:25 -0400

    handling-login

--- ui/src/pages/Login/Login.jsx
+++ ui/src/pages/Login/Login.jsx
@@ -5,6 +5,7 @@ import UIInput from "../../components/ui/FormUI/Field/Field";
 import UIForm from "../../components/ui/FormUI/Form/Form";
 import { Container } from "semantic-ui-react";
 import UIMessage from "../../components/ui/UIMessage/UIMessage";
+import axios from "axios";
 
 const Login = ({ props }) => {
   const [loading, setLoading] = useState(false);
@@ -12,15 +13,59 @@ const Login = ({ props }) => {
   const [isError, setIsError] = useState(false);
   const [inputs, setInputs] = useState({});
 
-  const onSubmitHandler = async (event) => {
-    event.preventDefault();
-    setLoading(true);
+const onSubmitHandler = async (event) => {
+  event.preventDefault();
+  setLoading(true);
 
+  //validate user's input
+  fieldValidation();
+
+  try {
     // ⚠️Handling login...
-    
-    //If the user is logged in successfully
-    // localStorage.setItem('isLoggedIn', true);
+    if (!isError) {
+      const loginPayload = {
+        usernameOrEmail: inputs.username,
+        password: inputs.password
+      };
+      const res = await axios.post("api/login", loginPayload);
+      console.log(res.data);
+      if (res.status === 200) {
+        //If the user is logged in successfully
+        alert(`You're successfully logged in ${inputs.username}!`);
+        
+        localStorage.setItem('isLoggedIn', true);  
+        setLoading(false);
+        setIsError(false);
+        //props.history.push('/home');
+        setTimeout(() => {
+          setMessage(`You're successfully logged in ${inputs.username}!`);
+        }, 5000);
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+    setMessage("Invalid credentials");
+    setLoading(false);
   }
+}
+
+const fieldValidation = () => {
+  //Validating user's input
+  for(const i in inputs){
+    if(!isError && i === "username" && inputs[i].length === 0){
+      setIsError(true);
+      setMessage("Username cannot be empty");
+      setLoading(false);
+      return;
+    }
+  if(!isError && i === "password" && inputs[i].length !== 8){
+      setIsError(true);
+      setMessage("Password must be 8 characters long");
+      setLoading(false);
+      return;
+    }
+  }
+}
 
   const onChangeHandler = (event) => {
     const name = event.target.name; // Gets the name of the field
